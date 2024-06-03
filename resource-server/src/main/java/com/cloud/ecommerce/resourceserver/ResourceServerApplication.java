83d6b19199a38f1d489bdbf6003af326165599b5
Tutku Ince <tutku.ince@outlook.com>
2024-06-03 17:33:17 +0300

    seeding the dummy data into database tables

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/ResourceServerApplication.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/ResourceServerApplication.java
@@ -1,7 +1,20 @@
 package com.cloud.ecommerce.resourceserver;
 
+import com.cloud.ecommerce.resourceserver.model.Brand;
+import com.cloud.ecommerce.resourceserver.model.Category;
+import com.cloud.ecommerce.resourceserver.model.Product;
+import com.cloud.ecommerce.resourceserver.repository.BrandRepository;
+import com.cloud.ecommerce.resourceserver.repository.CategoryRepository;
+import com.cloud.ecommerce.resourceserver.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class ResourceServerApplication {
@@ -10,4 +23,28 @@ public class ResourceServerApplication {
         SpringApplication.run(ResourceServerApplication.class, args);
     }
 
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private BrandRepository brandRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Bean
+    public CommandLineRunner seedDatabase() {
+        return args -> {
+            List<Brand> brands = List.of(new Brand("Adidas"), new Brand("Nike"), new Brand("New Balance"));
+            brandRepository.saveAll(brands);
+
+            List<Category> categories = List.of(new Category("Running"), new Category("Tennis"), new Category("Basketball"));
+            categoryRepository.saveAll(categories);
+
+            List<Product> products = List.of(
+                    new Product("AD101", "World Star", "Shoes for next century", new BigDecimal("195.5"), "shoe-1.jpg", true, 12, LocalDateTime.now(), LocalDateTime.now(), new Category(2L), new Brand(1L)),
+                    new Product("NI101", "White Line", "will make you world champ", new BigDecimal("293.5"), "shoe-2.jpg", true, 12, LocalDateTime.now(), LocalDateTime.now(), new Category(3L), new Brand(2L)),
+                    new Product("NE101", "530", "Run Forrest Run", new BigDecimal("95.5"), "shoe-3.jpg", true, 12, LocalDateTime.now(), LocalDateTime.now(), new Category(1L), new Brand(3L))
+            );
+            productRepository.saveAll(products);
+        };
+    }
 }
