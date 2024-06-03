2ae0785cf677eace6571bbc9c5e58c433dc90d70
Tutku Ince <tutku.ince@outlook.com>
2024-06-03 22:28:25 +0300

    ProductRepository

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/repository/ProductRepository.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/repository/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.cloud.ecommerce.resourceserver.repository;
 
 import com.cloud.ecommerce.resourceserver.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    public Page<Product> findAll(Specification<Product> specification, Pageable pageable);
+
+    public List<Product> findAll(Specification<Product> specification);
 }
