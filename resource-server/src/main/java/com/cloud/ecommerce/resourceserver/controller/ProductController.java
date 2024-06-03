9ec7a4f9b6c03426d347bf9eaa795ab2599744b8
Tutku Ince <tutku.ince@outlook.com>
2024-06-03 17:55:36 +0300

    updated

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/ProductController.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/ProductController.java
@@ -1,22 +1,40 @@
 package com.cloud.ecommerce.resourceserver.controller;
 
+import com.cloud.ecommerce.resourceserver.model.Brand;
+import com.cloud.ecommerce.resourceserver.model.Category;
+import com.cloud.ecommerce.resourceserver.repository.BrandRepository;
+import com.cloud.ecommerce.resourceserver.repository.CategoryRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/shop")
 public class ProductController {
 
-    @GetMapping(value = "/categories")
-    public List<String> getCategories() {
-        return List.of("Shoes", "Jackets", "Suits");
+    private final CategoryRepository categoryRepository;
+    private final BrandRepository brandRepository;
+
+    public ProductController(CategoryRepository categoryRepository, BrandRepository brandRepository) {
+        this.categoryRepository = categoryRepository;
+        this.brandRepository = brandRepository;
     }
 
-    @GetMapping(value = "/products")
-    public List<String> getProducts() {
-        return List.of("Shoe-01", "Jacket-01", "Suit-01");
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        if (!Objects.isNull(categories)) return ResponseEntity.ok(categories);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/brands")
+    public ResponseEntity<List<Brand>> getBrands() {
+        List<Brand> brands = brandRepository.findAll();
+        if (!Objects.isNull(brands)) return ResponseEntity.ok(brands);
+        return ResponseEntity.noContent().build();
     }
 }
