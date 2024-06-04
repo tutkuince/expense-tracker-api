0b7f52c57ab1e264ed4b12d1e3e2dc2458a7acec
Tutku Ince <tutku.ince@outlook.com>
2024-06-04 15:32:00 +0300

    update

--- ecommerce-standalone/src/app/app.component.ts
+++ ecommerce-standalone/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ecommerce-standalone';
+  pageTitle = 'Hello, World of Angular';
 }
