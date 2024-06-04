350d22b5ab9448998097fee876c50572fdae7410
Tutku Ince <tutku.ince@outlook.com>
2024-06-04 13:00:28 +0300

    updated for new urls

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/ProductController.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.cloud.ecommerce.resourceserver.controller;
 
+import com.cloud.ecommerce.resourceserver.dto.ProductResponseDto;
+import com.cloud.ecommerce.resourceserver.dto.ProductResponseListDto;
 import com.cloud.ecommerce.resourceserver.model.Brand;
 import com.cloud.ecommerce.resourceserver.model.Category;
 import com.cloud.ecommerce.resourceserver.repository.BrandRepository;
 import com.cloud.ecommerce.resourceserver.repository.CategoryRepository;
+import com.cloud.ecommerce.resourceserver.service.ProductService;
+import com.cloud.ecommerce.resourceserver.specification.ProductSpecParams;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +23,12 @@ public class ProductController {
 
     private final CategoryRepository categoryRepository;
     private final BrandRepository brandRepository;
+    private final ProductService productService;
 
-    public ProductController(CategoryRepository categoryRepository, BrandRepository brandRepository) {
+    public ProductController(CategoryRepository categoryRepository, BrandRepository brandRepository, ProductService productService) {
         this.categoryRepository = categoryRepository;
         this.brandRepository = brandRepository;
+        this.productService = productService;
     }
 
     @GetMapping("/categories")
@@ -36,5 +43,17 @@ public class ProductController {
         List<Brand> brands = brandRepository.findAll();
         if (!Objects.isNull(brands)) return ResponseEntity.ok(brands);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<ProductResponseListDto> getProducts(ProductSpecParams params) {
+        ProductResponseListDto productList = productService.getProductList(params);
+        return ResponseEntity.ok(productList);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable long id) {
+        ProductResponseDto responseDto = productService.getProductById(id);
+        return ResponseEntity.ok(responseDto);
     }
 }
