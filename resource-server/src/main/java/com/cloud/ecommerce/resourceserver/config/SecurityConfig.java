5bee05f681e82d90305187582f8e4fd196090596
Tutku Ince <tutku.ince@outlook.com>
2024-06-05 15:03:15 +0300

    update security for cqrs

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/config/SecurityConfig.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/config/SecurityConfig.java
@@ -1,16 +1,38 @@
 package com.cloud.ecommerce.resourceserver.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(List.of("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }));
+        /*http.authorizeHttpRequests(
                 (authorize) -> authorize
                         .requestMatchers(
                                 "/api/shop/brands",
@@ -18,7 +40,7 @@ public class SecurityConfig {
                                 "/api/shop/products",
                                 "/api/shop/products/**"
                                 )
-                        .permitAll());
+                        .permitAll());*/
         return http.build();
     }
 }
