28064e161c896cdc9e6f6a7808baf4000f3d3d38
Tutku Ince <tutku.ince@outlook.com>
2024-06-06 20:54:17 +0300

    BasketController

---
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/BasketController.java
@@ -0,0 +1,43 @@
+package com.cloud.ecommerce.resourceserver.controller;
+
+import com.cloud.ecommerce.resourceserver.model.Basket;
+import com.cloud.ecommerce.resourceserver.model.BasketData;
+import com.cloud.ecommerce.resourceserver.repository.BasketRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/basket")
+public class BasketController {
+    private final BasketRepository basketRepository;
+
+    public BasketController(BasketRepository basketRepository) {
+        this.basketRepository = basketRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Basket> createBasket(@RequestBody BasketData basketData) {
+        Basket basket = new Basket(basketData.getId());
+        basket.setItems(basket.getItems());
+        basketRepository.save(basket);
+        return ResponseEntity.ok(basket);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Basket> getBasketById(@PathVariable String id) {
+        Optional<Basket> optionalBasket = basketRepository.findById(id);
+        return optionalBasket.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBasketById(@PathVariable String id) {
+        Optional<Basket> optionalBasket = basketRepository.findById(id);
+        if (optionalBasket.isPresent()) {
+            basketRepository.delete(optionalBasket.get());
+            return ResponseEntity.ok(id);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
