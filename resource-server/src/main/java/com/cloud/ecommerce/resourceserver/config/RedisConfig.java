49f84592f0ad2ee50891b8f92365db045efeb759
Tutku Ince <tutku.ince@outlook.com>
2024-06-06 17:52:40 +0300

    RedisConfig

---
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.cloud.ecommerce.resourceserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("localhost");
+        connectionFactory.setPort(6380);
+        return connectionFactory;
+    }
+}
