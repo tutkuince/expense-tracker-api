d4e0e7fea889a12383e2f483c11c270060d5ddde
Tutku Ince <tutku.ince@outlook.com>
2024-06-06 20:55:08 +0300

    BasketRepository

---
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/repository/BasketRepository.java
@@ -0,0 +1,10 @@
+package com.cloud.ecommerce.resourceserver.repository;
+
+import com.cloud.ecommerce.resourceserver.model.Basket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasketRepository extends CrudRepository<Basket, String> {
+
+}
