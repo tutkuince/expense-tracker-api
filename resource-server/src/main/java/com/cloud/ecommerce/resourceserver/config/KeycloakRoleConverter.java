3d13b877fffca6b28bb548c6952e1f8e123900ee
Tutku Ince <tutku.ince@outlook.com>
2024-06-10 17:15:04 +0300

    Keycloak config

---
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/config/KeycloakRoleConverter.java
@@ -0,0 +1,28 @@
+package com.cloud.ecommerce.resourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Configuration
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm access");
+        if (Objects.isNull(realmAccess) || realmAccess.isEmpty()) return new ArrayList<>();
+        return ((List<String>) realmAccess.get("roles"))
+                .stream().map(roleName -> "ROLE_")
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public <U> Converter<Jwt, U> andThen(Converter<? super Collection<GrantedAuthority>, ? extends U> after) {
+        return Converter.super.andThen(after);
+    }
+}
