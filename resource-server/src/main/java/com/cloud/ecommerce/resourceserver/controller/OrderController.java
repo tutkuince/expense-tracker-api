3bf2c665f000ecae2adb39749d48a1a4a6bf36bb
Tutku Ince <tutku.ince@outlook.com>
2024-06-10 17:17:58 +0300

    fixed bug

--- resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/OrderController.java
+++ resource-server/src/main/java/com/cloud/ecommerce/resourceserver/controller/OrderController.java
@@ -1,14 +1,16 @@
 package com.cloud.ecommerce.resourceserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/orders")
+@RequestMapping(value = "/api")
 public class OrderController {
 
+    @GetMapping("/orders")
     public List<String> getOrders() {
         return List.of("Order-1", "Order-2", "Order-3");
     }
